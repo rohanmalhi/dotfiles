cc38fd1f6437cab900eff632bb795476baf683d6
Rohan Malhi <rohan.s.malhi@gmail.com>
2025-07-19 12:26:34 -0700

    Automated by Cabinet

---
+++ .config/nvim/init.lua
@@ -0,0 +1,12 @@
+require "opts"
+require "launch"
+
+spec("plugin.colorscheme")
+spec("plugin.treesitter")
+spec("plugin.telescope")
+spec("plugin.fugitive")
+spec("plugin.lsp-zero")
+spec("plugin.mason")
+
+require "plugin.lazy"
+require "keymaps"
